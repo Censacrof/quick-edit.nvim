fdbea2cc8e488a496e8115877b83ccd1fb9e9148
Francesco Galisi <galisifrancesco@gmail.com>
2024-04-26 11:24:38 +0200

    Return setup function

--- lua/quick-edit/init.lua
+++ lua/quick-edit/init.lua
@@ -1,1 +1,12 @@
-print("Hello World")
+local augroup = vim.api.nvim_create_augroup("quick-edit", { clear = true })
+
+local function main()
+	print("Hello World")
+end
+
+local function setup()
+	vim.api.nvim_create_autocmd("VimEnter",
+		{ group = augroup, desc = "Edit quickfix list as a regular buffer", once = true, callback = main })
+end
+
+return { setup = setup }
