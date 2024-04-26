9aa33abcf9758d3d1ed7c62b219e6d2f8d4db038
Francesco Galisi <galisifrancesco@gmail.com>
2024-04-26 15:24:39 +0200

    Init buffer with contents from quickfix list

--- lua/quick-edit/init.lua
+++ lua/quick-edit/init.lua
@@ -6,7 +6,7 @@ local function get_quick_edit_buf()
 		return Buf
 	end
 
-	Buf = vim.api.nvim_create_buf(true, false)
+	Buf = vim.api.nvim_create_buf(true, true)
 	if Buf == 0 then
 		error("Error creating quick-edit buffer")
 	end
@@ -32,7 +32,36 @@ local function open_quick_edit_window()
 	vim.api.nvim_win_set_buf(0, buf)
 end
 
+local function init_quick_edit_with_quickfix()
+	local buf = get_quick_edit_buf()
+
+	local qf_list = vim.fn.getqflist()
+	local lines = {}
+
+	-- Print each entry in the quickfix list
+	for idx, entry in ipairs(qf_list) do
+		-- bufnr = 10,
+		-- col = 5,
+		-- end_col = 11,
+		-- end_lnum = 8,
+		-- lnum = 8,
+		-- module = "",
+		-- nr = 0,
+		-- pattern = "",
+		-- text = 'vim.keymap.set("n", "<leader>a2", function() ui.nav_file(2) end, { desc = "Harpoon: go to file 2" })',
+		-- type = "",
+		-- valid = 1,
+		-- vcol = 0
+		local filename = vim.api.nvim_buf_get_name(entry.bufnr)
+		lines[#lines + 1] = string.format("%d|%s:%d:%d|%s", idx, filename or "", entry.lnum, entry.col, entry
+		.text)
+	end
+
+	vim.api.nvim_buf_set_lines(buf, 0, -1, true, lines)
+end
+
 local function quick_edit_open()
+	init_quick_edit_with_quickfix()
 	open_quick_edit_window()
 end
 
