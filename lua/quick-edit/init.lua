e953e14e6fce7f2808f07ab89bf2e40b3539326e
Francesco Galisi <galisifrancesco@gmail.com>
2024-04-26 12:59:36 +0200

    Reuse same window and buffer

--- lua/quick-edit/init.lua
+++ lua/quick-edit/init.lua
@@ -1,11 +1,39 @@
 local augroup = vim.api.nvim_create_augroup("quick-edit", { clear = true })
 
-local function quick_edit_open()
-	local buf = vim.api.nvim_create_buf(true, false)
-	vim.api.nvim_buf_set_name(buf, "*quick-edit*")
+Buf = 0
+local function get_quick_edit_buf()
+	if Buf ~= 0 then
+		return Buf
+	end
+
+	Buf = vim.api.nvim_create_buf(true, false)
+	if Buf == 0 then
+		error("Error creating quick-edit buffer")
+	end
+
+	vim.api.nvim_buf_set_name(Buf, "quick-edit")
 	vim.api.nvim_set_option_value("buftype", "nofile", {})
 
+	return Buf
+end
+
+local function open_quick_edit_window()
+	local buf = get_quick_edit_buf()
+
+	local win_list = vim.fn.win_findbuf(buf)
+
+	if #win_list > 0 then
+		return
+	end
+
+	-- make sure current window is quickfix list
+	vim.cmd.copen()
+
 	vim.api.nvim_win_set_buf(0, buf)
+end
+
+local function quick_edit_open()
+	open_quick_edit_window()
 end
 
 local function main()
